f938ce00a25b4324ff6aea9790ff5e7561327487
KarrynS <karrynsaw@gmail.com>
2021-02-14 21:56:54 +1100

    established directory structure

---
+++ models/burger.js
@@ -0,0 +1,10 @@
+const orm = requir("../config/orm.js");
+
+
+
+//TO DO: 
+/*
+* Also inside `burger.js`, create the code that will call the ORM functions using burger specific input for the ORM.
+*/
+
+module.exports = burger;
--- server.js
+++ server.js
@@ -1,1 +1,26 @@
 const express = require("express");
+
+//Set up express app
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+// Set up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve static content for the app
+app.use(express.static("public"));
+
+
+// Set Handlebars.
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Import routes and give the server access to them.
+const routes = require('./controllers/burgers_controller.js');
+
+app.use(routes);
+
+app.listen(PORT, () => console.log(`App now listening at localhost:${PORT}`));
